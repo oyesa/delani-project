5c8e9aa98ac6470110f67bb046898bc5597b1574
oyesa <mercyoyesa2018@gmail.com>
2022-03-12 20:40:42 +0300

    update front end logic

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  $("form#delaniForm").submit(function(event){
+    event.preventDefault();
+
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+
+    if($("input#MERGE1").val() && $("input#MERGE0".val()){
+      alert (name + ${Your message has been recieved. Thank you for reaching out to us.});
+    }
+    else {
+      alert("Please enter your name and email");
+    }
+  });
+});
