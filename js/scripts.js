13d28f0b7dca3bac0aa40bc5648706c5333480b6
oyesa <mercyoyesa2018@gmail.com>
2022-03-13 16:44:21 +0300

    update contact function in js

--- js/scripts.js
+++ js/scripts.js
@@ -20,18 +20,33 @@ $(document).ready(function() {
 
 // Form function
 
-$(document).ready(function(){
-  $("form#delaniForm").submit(function(e){
-  e.preventDefault();
-  
-  var name = $("input#MERGE1").val();
+  var username = $("input#MERGE1").val();
   var email = $("input#MERGE0").val();
   var message = $("textarea#comment").val();
+
+$(document).ready(function() {
+  $("button#submitBtn").click(function() {
+    if($("input#MERGE1").val() && $("input#MERGE0").val()) {
+        alert(username + "Your message has been recieved. Thank you for reaching out to us.");
+        }
+        else {
+        alert("Please enter your name and email");
+        }
+  });
+});
+
+// $(document).ready(function(){
+//   $("form#delaniForm").submit(function(e){
+//   e.preventDefault();
   
-  if($("input#MERGE1").val() && $("input#MERGE0").val()) {
-  alert(name + "Your message has been recieved. Thank you for reaching out to us.");
-  }
-  else {
-  alert("Please enter your name and email");
-  }
+//   var name = $("input#MERGE1").val();
+//   var email = $("input#MERGE0").val();
+//   var message = $("textarea#comment").val();
+  
+//   if($("input#MERGE1").val() && $("input#MERGE0").val()) {
+//   alert(name + "Your message has been recieved. Thank you for reaching out to us.");
+//   }
+//   else {
+//   alert("Please enter your name and email");
+//   }
   
