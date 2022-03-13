ada7ea1b94a0d3efe442712888a7d43a3ba57bea
oyesa <mercyoyesa2018@gmail.com>
2022-03-13 12:38:32 +0300

    add toggle function to what we do

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,18 @@ $(document).ready(function(){
     var email = $("input#MERGE0").val();
     var message = $("textarea#comment").val();
 
-    if($("input#MERGE1").val() && $("input#MERGE0".val()){
+    if($("input#MERGE1").val() && $("input#MERGE0".val()) {
       alert (name + ", Your message has been recieved. Thank you for reaching out to us.");
     }
     else {
       alert("Please enter your name and email");
     }
+  });
+});
+
+// add toggle effect on what-we-do section
+$(document).ready(function() {
+  $("#design").click(function() {
+    $("p").toggle();
   });
 });
